1d9acb314218a59779b80693d16ea7145e3ae87d
filippo <filippo.brunnelleschi@gmail.com>
2025-04-05 17:52:57 +0200

    Finish es 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let t="";
+    let i=0;
+    let s= str.split("");
+    let f=s.length;
+    while (i<f)
+    {
+        t=s[i];
+        s[i]=s[f];
+        s[f]=t;
+        i++;
+        f--;
+    }
+    str= s.join("");
+    return str;
 };
 
 // Do not edit below this line
