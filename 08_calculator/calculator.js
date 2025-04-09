be95571b6194b30ff91d054bf5833759bb38b076
filippo <filippo.brunnelleschi@gmail.com>
2025-04-09 15:43:01 +0200

    Finish es 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
+const add = function(a,b) {
+	return a+b;
+};
+
+const subtract = function(a,b) {
+	return a-b;
+};
+
+const sum = function(a) {
+  let s=0;
+  for (let  i=0; i<a.length; i++)
+  {
+    s+=a[i];
+  }
+  return s;
+};
+
+const multiply = function(a) {
+  let s=1;
+  for (let  i=0; i<a.length; i++)
+  {
+    s*=a[i];
+  }
+  return s;
+
+};
+
+const power = function(b,e) {
+  return b**e;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+  let s=1;
+  for (let i=a; i>0; i--)
+  {
+    s*=i;
+  }
+  return s;
 };
 
 // Do not edit below this line
