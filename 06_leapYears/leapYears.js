172db898966fd0e4b872da8abfb39a0506087c53
filippo <filippo.brunnelleschi@gmail.com>
2025-04-09 14:55:39 +0200

    Finish es 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+const leapYears = function(n) {
+    if (n%4==0)
+    {
+        if (n%100==0 && n%400!=0)
+        {
+            return false;
+        }
+        return true;
+    }
+    return false;
 
 };
 
