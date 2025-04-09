b650dd0b130390a04075f3e228bb48759f1110d6
filippo <filippo.brunnelleschi@gmail.com>
2025-04-09 14:35:14 +0200

    Finish es 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
-};
+
+    const removeFromArray = function(a, ...v) {
+
+        const args= [...v];
+        {
+            for (let i=a.length-1; i>=0; i--)
+                {
+                    for (let j=0; j<v.length; j++)
+                    {
+                        if (a[i]===v[j])
+                            {
+                                a.splice(i,1);
+                            }
+                    }
+                }
+        }
+
+
+        
+        return a;
+ };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
