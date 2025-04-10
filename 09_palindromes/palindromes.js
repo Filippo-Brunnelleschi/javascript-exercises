67dd00e770d1907b4f002bbb22b608703bf38987
filippo <filippo.brunnelleschi@gmail.com>
2025-04-10 17:33:43 +0200

    Finish es 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (s) {
 
+  const alpha="abcdefghijklmnopqrstuvwxyz0123456789";
+
+  const cleanString = s
+    .toLowerCase()
+    .split("")
+    .filter((character) => alpha.includes(character))
+    .join("");
+
+  const reverseString=cleanString.split("").reverse().join("");
+
+  return cleanString===reverseString;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
