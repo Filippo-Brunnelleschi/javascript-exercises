3a87e8e155b61c6ead2b198beacc70e39779b378
filippo <filippo.brunnelleschi@gmail.com>
2025-04-13 16:07:34 +0200

    Finish es 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+
+    if (n>=0)
+    {
+    let map = new Map();
+
+    if (n==0){return 0;}
+
+  if (n==1 || n==2)
+    {
+        return 1;
+    }
+
+    if (n in map)
+    {
+        return map.n;
+    }
+    map.n=fibonacci(n-1)+ fibonacci(n-2);
+    return map.n;
+    }
+    else {return "OOPS";}
 };
 
 // Do not edit below this line
